03f181fe3ae6c42084302f7d9c428b9816b45f0b
ireneelizabethsabu <irenepalanatt@gmail.com>
2021-08-25 18:53:02 +0530

    Adding moon details

---
+++ src/components/DayDetail/MoonDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Col } from 'react-bootstrap'; 
+import {ReactComponent as Moonrise} from '../../assets/moonrise.svg';
+import {ReactComponent as Moonset} from '../../assets/moonset.svg';
+import {getHours,getMinutes,getmoonPhase} from '../../helpers/index';
+
+const MoonDetails = ({rise,set,phase,timezone}) => {
+    return(
+        <Col className="pl-0" sm="4">
+            <div className="font_s border-detail py-2">Moonrise</div>
+            <Moonrise/>
+            <span className="ml-3 font_m">{getHours(rise,timezone)} : {getMinutes(rise,timezone)}</span>
+            <div className="font_s py-2">Moonset</div>
+            <Moonset/>
+            <span className="ml-3 font_m">{getHours(set,timezone)} : {getMinutes(set,timezone)}</span>
+            <div className="font_s py-2">Moon Phase</div>
+            <span>{getmoonPhase(phase)}</span>
+        </Col>
+    )
+}
+
+export default MoonDetails;
